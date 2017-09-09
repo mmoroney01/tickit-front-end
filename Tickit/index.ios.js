f003327e4519db27a4f177fe4ddfd6a08cf0e854
MMoroney <email": null>
2017-09-09 15:00:18 -0500

    added save location futton with alert functionalty

--- Tickit/index.ios.js
+++ Tickit/index.ios.js
@@ -4,9 +4,181 @@
  * @flow
  */
 
+
 import React, { Component } from 'react';
-import MapView from 'react-native-maps';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import MapView, { MAP_TYPES } from 'react-native-maps';
+import { Alert, AppRegistry, StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+
+class DisplayLatLng extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+    this.onSubmitPressed = this.onSubmitPressed.bind(this);
+  }
+
+
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  jumpRandom() {
+    this.setState({ region: this.randomRegion() });
+  }
+
+  animateRandom() {
+    this.map.animateToRegion(this.randomRegion());
+  }
+
+  animateRandomCoordinate() {
+    this.map.animateToCoordinate(this.randomCoordinate());
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
+
+
+  randomCoordinate() {
+    const region = this.state.region;
+    return {
+      latitude: region.latitude + ((Math.random() - 0.5) * (region.latitudeDelta / 2)),
+      longitude: region.longitude + ((Math.random() - 0.5) * (region.longitudeDelta / 2)),
+    };
+  }
+
+  randomRegion() {
+    return {
+      ...this.state.region,
+      ...this.randomCoordinate(),
+    };
+  }
+
+  getLat(){
+    return JSON.stringify(this.state.region.latitude);
+  }
+
+  getLong(){
+    return JSON.stringify(this.state.region.longitude);
+  }
+
+  onSubmitPressed() {
+      return JSON.stringify({this.state.region.latitude, this.state.region.longitude});
+      Alert.alert(
+          'Thank you',
+          "Your location has been saved."
+      );
+    }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          provider={this.props.provider}
+          ref={ref => { this.map = ref; }}
+          mapType={MAP_TYPES.HYBRID}
+          style={styles.map}
+          initialRegion={this.state.region}
+          onRegionChange={region => this.onRegionChange(region)}
+        />
+        <View style={[styles.bubble, styles.latlng]}>
+          <Text style={{ textAlign: 'center' }}>
+            {this.state.region.latitude.toPrecision(7)},
+            {this.state.region.longitude.toPrecision(7)}
+          </Text>
+
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={async () => this.animateRandom()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text style={styles.buttonText}>Animate (Region)</Text>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity
+            onPress={async () => this.onSubmitPressed()}
+
+            style={[styles.bubble, styles.button]}
+          >
+            <Text style={styles.buttonText}>Submit Location</Text>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity
+            onPress={() => this.animateRandomCoordinate()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text style={styles.buttonText}>Animate (Coordinate)</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+DisplayLatLng.propTypes = {
+  provider: MapView.ProviderPropType,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 100,
+    paddingHorizontal: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+  buttonText: {
+    textAlign: 'center',
+  },
+});
 
 const mapStyles = StyleSheet.create({
   container: {
@@ -28,38 +200,53 @@ const mapStyles = StyleSheet.create({
 });
 
 export default class Tickit extends Component {
-  render() {
-    return (
-      <MapView
-        style={mapStyles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-      />
-    );
+
+  state = {
+    mapRegion: null,
+    lastLat: null,
+    lastLong: null,
   }
+
+  componentDidMount() {
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      // Create the object to update this.state.mapRegion through the onRegionChange function
+      let region = {
+        latitude:       position.coords.latitude,
+        longitude:      position.coords.longitude,
+        latitudeDelta:  0.00922*1.5,
+        longitudeDelta: 0.00421*1.5
+      }
+      this.onRegionChange(region, region.latitude, region.longitude);
+    });
+  }
+
+  onRegionChange(region, lastLat, lastLong) {
+    this.setState({
+      mapRegion: region,
+      // If there are no new values set use the the current ones
+      lastLat: lastLat || this.state.lastLat,
+      lastLong: lastLong || this.state.lastLong
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  onMapPress(e) {
+    console.log(e.nativeEvent.coordinate.longitude);
+    let region = {
+      latitude:       e.nativeEvent.coordinate.latitude,
+      longitude:      e.nativeEvent.coordinate.longitude,
+      latitudeDelta:  0.00922*1.5,
+      longitudeDelta: 0.00421*1.5
+    }
+    this.onRegionChange(region, region.latitude, region.longitude);
+  }
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
 
-AppRegistry.registerComponent('Tickit', () => Tickit);
+
+AppRegistry.registerComponent('Tickit', () => DisplayLatLng);
