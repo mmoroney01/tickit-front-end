607f9be0a1c932d243e21f6cd8a623d981fe894f
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-09-10 14:37:59 -0500

    hardcode ploygon cordnates for ward to shwo proof of concept

--- Tickit/index.ios.js
+++ Tickit/index.ios.js
@@ -9,6 +9,7 @@ import { AppRegistry, Text, View, TouchableOpacity } from 'react-native';
 import Login from './src/pages/Login';
 import Registration from './src/pages/Registration';
 import DefaultMap from './src/pages/default_map';
+import PolygonMap from './src/pages/polygon_overlay';
 
 export default class Tickit extends Component {
   constructor(props) {
@@ -30,7 +31,7 @@ export default class Tickit extends Component {
     if (2 != 2) {
       return <Login />;
     } else {
-      return <DefaultMap />;
+      return <PolygonMap />;
     }
   }
 }
