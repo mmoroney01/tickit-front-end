ee8077e5ad5bcaf956aa6d044039d7ef07250dc3
MMoroney <email": null>
2017-09-10 20:35:35 -0500

    add scroll wheel for datetime

--- Tickit/src/pages/mapRender.js
+++ Tickit/src/pages/mapRender.js
@@ -1,12 +1,17 @@
+'use strict';
+
 import React, { Component } from 'react';
 import MapView, { MAP_TYPES } from 'react-native-maps';
-import { Alert, AppRegistry, StyleSheet, Text, Image, View, Dimensions, TouchableOpacity } from 'react-native';
+import { Alert, AppRegistry, StyleSheet, Text, TextInput, Image, View,  Dimensions, DatePickerIOS, TouchableOpacity } from 'react-native';
 import { Icon } from 'react-native-elements';
 import Fontawesomeicons from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const { width, height } = Dimensions.get('window');
 const ASPECT_RATIO = width / height;
+
+var DateTimePicker = require('react-native-datetime').default;
+var Button = require('@remobile/react-native-simple-button');
 
 class DisplayLatLng extends React.Component {
   constructor(props) {
@@ -20,10 +25,13 @@ class DisplayLatLng extends React.Component {
             longitudeDelta: 0.0421
           },
       startingLatitude: 37.78825,
-      startingLongitude: -122.4324
+      startingLongitude: -122.4324,
+      date: new Date(),
+      timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60
     };
     this.onSubmitPressed = this.onSubmitPressed.bind(this);
     this.onFindPressed = this.onFindPressed.bind(this);
+    this.onDateChange = this.onDateChange.bind(this);
   }
 
   onRegionChange(region) {
@@ -34,21 +42,29 @@ class DisplayLatLng extends React.Component {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         var initialPosition = position;
-        console.log(initialPosition);
-        console.log(this.state.region);
-        console.log(initialPosition.coords.latitude)
         this.setState({ region: { latitude: initialPosition.coords.latitude, longitude: initialPosition.coords.longitude }});
       },
       (error) => this.setState({ error: error.message }),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
     );
 
-    console.log(this.state.region);
-
     this.watchID = navigator.geolocation.watchPosition((position) => {
     var lastPosition = JSON.stringify(position);
     this.setState({lastPosition});
   });
+  }
+
+  onDateChange(date) {
+    this.setState({date: date});
+  }
+
+
+  onTimezoneChange(event) {
+    var offset = parseInt(event.nativeEvent.text, 10);
+    if (isNaN(offset)) {
+      return;
+    }
+    this.setState({timeZoneOffsetInHours: offset});
   }
 
   onSubmitPressed() {
@@ -84,7 +100,6 @@ class DisplayLatLng extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-
         <MapView
           showsUserLocation={true}
           showsMyLocationButton={true}
@@ -110,9 +125,23 @@ class DisplayLatLng extends React.Component {
             name='rowing'/>
         </View>
 
+        <View>
+        <DatePickerIOS
+          date={this.state.date}
+          mode="datetime"
+          timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+          onDateChange={this.onDateChange}
+        />
+        <WithLabel>
+          <Text>{
+            this.state.date.toLocaleDateString() +
+            ' ' +
+            this.state.date.toLocaleTimeString()
+          }</Text>
+        </WithLabel>
+      </View>
 
         <View style={styles.buttonContainer}>
-
             <TouchableOpacity
               onPress={this.onFindPressed}
               style={[styles.bubble, styles.button]}>
@@ -125,11 +154,39 @@ class DisplayLatLng extends React.Component {
             >
               <Text style={styles.buttonText}>Submit Location</Text>
             </TouchableOpacity>
+
         </View>
       </View>
     );
   }
 }
+
+var Heading = React.createClass({
+  render() {
+    return (
+      <View style={buttonStyles.headingContainer}>
+        <Text style={buttonStyles.heading}>
+          {this.props.label}
+        </Text>
+      </View>
+    );
+  }
+});
+
+var WithLabel = React.createClass({
+  render() {
+    return (
+      <View style={buttonStyles.labelContainer}>
+        <View style={buttonStyles.labelView}>
+          <Text style={buttonStyles.label}>
+            {this.props.label}
+          </Text>
+        </View>
+        {this.props.children}
+      </View>
+    );
+  }
+});
 
 const SIZE = 35;
 const HALO_RADIUS = 6;
@@ -137,6 +194,38 @@ const ARROW_SIZE = 7;
 const ARROW_DISTANCE = 6;
 const HALO_SIZE = SIZE + HALO_RADIUS;
 const HEADING_BOX_SIZE = HALO_SIZE + ARROW_SIZE + ARROW_DISTANCE;
+
+
+const buttonStyles = StyleSheet.create({
+  textinput: {
+    height: 26,
+    width: 50,
+    borderWidth: 0.5,
+    borderColor: '#0f0f0f',
+    padding: 4,
+    fontSize: 13,
+  },
+  labelContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 2,
+  },
+  labelView: {
+    marginRight: 10,
+    paddingVertical: 2,
+  },
+  label: {
+    fontWeight: '500',
+  },
+  headingContainer: {
+    padding: 4,
+    backgroundColor: '#f6f7f8',
+  },
+  heading: {
+    fontWeight: '500',
+    fontSize: 14,
+  },
+});
 
 const markerStyles = StyleSheet.create({
   mapMarker: {
