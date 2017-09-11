25cad3a2337018fc63835a9dbc8b8b6ccd41b686
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-09-11 11:11:49 -0500

    application shows overlay if in street sweeping zone

--- Tickit/src/pages/mapRender.js
+++ Tickit/src/pages/mapRender.js
@@ -2,7 +2,18 @@
 
 import React, { Component } from 'react';
 import MapView, { MAP_TYPES } from 'react-native-maps';
-import { Alert, AppRegistry, StyleSheet, Text, TextInput, Image, View,  Dimensions, DatePickerIOS, TouchableOpacity } from 'react-native';
+import {
+  Alert,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  Image,
+  View,
+  Dimensions,
+  DatePickerIOS,
+  TouchableOpacity
+} from 'react-native';
 import { Icon } from 'react-native-elements';
 import Fontawesomeicons from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -16,15 +27,16 @@ class DisplayLatLng extends React.Component {
 
     this.state = {
       region: {
-            latitude: 41.87625540000001,
-            longitude: -87.65306249999998,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          },
+        latitude: 41.87625540000001,
+        longitude: -87.65306249999998,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      },
       startingLatitude: 37.78825,
       startingLongitude: -122.4324,
+      polygons: [],
       date: new Date(),
-      timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60
+      timeZoneOffsetInHours: -1 * new Date().getTimezoneOffset() / 60
     };
     this.onSubmitPressed = this.onSubmitPressed.bind(this);
     this.onFindPressed = this.onFindPressed.bind(this);
@@ -37,31 +49,32 @@ class DisplayLatLng extends React.Component {
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      (position) => {
+      position => {
         var initialPosition = position;
-        this.setState({ region: { latitude: initialPosition.coords.latitude, longitude: initialPosition.coords.longitude }});
+        this.setState({
+          region: { latitude: initialPosition.coords.latitude, longitude: initialPosition.coords.longitude }
+        });
       },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+      error => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     );
 
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-    var lastPosition = JSON.stringify(position);
-    this.setState({lastPosition});
-  });
+    this.watchID = navigator.geolocation.watchPosition(position => {
+      var lastPosition = JSON.stringify(position);
+      this.setState({ lastPosition });
+    });
   }
 
   onDateChange(date) {
-    this.setState({date: date});
+    this.setState({ date: date });
   }
-
 
   onTimezoneChange(event) {
     var offset = parseInt(event.nativeEvent.text, 10);
     if (isNaN(offset)) {
       return;
     }
-    this.setState({timeZoneOffsetInHours: offset});
+    this.setState({ timeZoneOffsetInHours: offset });
   }
 
   onSubmitPressed() {
@@ -78,7 +91,7 @@ class DisplayLatLng extends React.Component {
     })
       .then(response => response.json())
       .then(responseData => {
-        console.log(responseData.response);
+        this.setState({ polygons: responseData.coordinates });
         Alert.alert(responseData.response);
       })
       .done();
@@ -102,38 +115,48 @@ class DisplayLatLng extends React.Component {
         <MapView
           showsUserLocation={true}
           showsMyLocationButton={true}
-
           showsTraffic={true}
           provider={this.props.provider}
-          ref={ref => { this.map = ref; }}
+          ref={ref => {
+            this.map = ref;
+          }}
           mapType={MAP_TYPES.HYBRID}
           style={styles.map}
           initialRegion={{
             latitude: 41.87625540000001,
             longitude: -87.65306249999998,
-            latitudeDelta:  0.0922,
+            latitudeDelta: 0.0922,
             longitudeDelta: 0.0421
           }}
           onRegionChange={region => this.onRegionChange(region)}
         >
+          <MapView.Polygon
+            coordinates={this.state.polygons}
+            strokeColor="#F00"
+            fillColor="rgba(255,0,0,0.5)"
+            strokeWidth={1}
+          />
         </MapView>
-
-        <View pointerEvents="none" style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, alignItems: 'center', justifyContent: 'center', backgroundColor: 'transparent'}}>
-            <Icon
-            pointerEvents="none"
-            name='rowing'/>
+        <View
+          pointerEvents="none"
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'transparent'
+          }}
+        >
+          <Icon pointerEvents="none" name="rowing" />
         </View>
 
         <View style={styles.buttonContainer}>
-
-
-            <TouchableOpacity
-              onPress={async () => this.onSubmitPressed()}
-              style={[styles.bubble, styles.button]}
-            >
-              <Text style={styles.buttonText}>Submit Location</Text>
-            </TouchableOpacity>
-
+          <TouchableOpacity onPress={async () => this.onSubmitPressed()} style={[styles.bubble, styles.button]}>
+            <Text style={styles.buttonText}>Submit Location</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -144,9 +167,7 @@ var Heading = React.createClass({
   render() {
     return (
       <View style={buttonStyles.headingContainer}>
-        <Text style={buttonStyles.heading}>
-          {this.props.label}
-        </Text>
+        <Text style={buttonStyles.heading}>{this.props.label}</Text>
       </View>
     );
   }
@@ -157,9 +178,7 @@ var WithLabel = React.createClass({
     return (
       <View style={buttonStyles.labelContainer}>
         <View style={buttonStyles.labelView}>
-          <Text style={buttonStyles.label}>
-            {this.props.label}
-          </Text>
+          <Text style={buttonStyles.label}>{this.props.label}</Text>
         </View>
         {this.props.children}
       </View>
@@ -174,7 +193,6 @@ const ARROW_DISTANCE = 6;
 const HALO_SIZE = SIZE + HALO_RADIUS;
 const HEADING_BOX_SIZE = HALO_SIZE + ARROW_SIZE + ARROW_DISTANCE;
 
-
 const buttonStyles = StyleSheet.create({
   textinput: {
     height: 26,
@@ -182,38 +200,38 @@ const buttonStyles = StyleSheet.create({
     borderWidth: 0.5,
     borderColor: '#0f0f0f',
     padding: 4,
-    fontSize: 13,
+    fontSize: 13
   },
   labelContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    marginVertical: 2,
+    marginVertical: 2
   },
   labelView: {
     marginRight: 10,
-    paddingVertical: 2,
+    paddingVertical: 2
   },
   label: {
-    fontWeight: '500',
+    fontWeight: '500'
   },
   headingContainer: {
     padding: 4,
-    backgroundColor: '#f6f7f8',
+    backgroundColor: '#f6f7f8'
   },
   heading: {
     fontWeight: '500',
-    fontSize: 14,
-  },
+    fontSize: 14
+  }
 });
 
 const markerStyles = StyleSheet.create({
   mapMarker: {
-    zIndex: 1000,
+    zIndex: 1000
   },
   // The container is necessary to protect the markerHalo shadow from clipping
   container: {
     width: HEADING_BOX_SIZE,
-    height: HEADING_BOX_SIZE,
+    height: HEADING_BOX_SIZE
   },
   heading: {
     position: 'absolute',
@@ -221,7 +239,7 @@ const markerStyles = StyleSheet.create({
     left: 0,
     width: HEADING_BOX_SIZE,
     height: HEADING_BOX_SIZE,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   headingPointer: {
     width: 0,
@@ -235,7 +253,7 @@ const markerStyles = StyleSheet.create({
     borderTopColor: 'transparent',
     borderRightColor: 'transparent',
     borderBottomColor: 'red',
-    borderLeftColor: 'transparent',
+    borderLeftColor: 'transparent'
   },
   markerHalo: {
     position: 'absolute',
@@ -251,8 +269,8 @@ const markerStyles = StyleSheet.create({
     shadowRadius: 2,
     shadowOffset: {
       height: 0,
-      width: 0,
-    },
+      width: 0
+    }
   },
   marker: {
     justifyContent: 'center',
@@ -260,44 +278,44 @@ const markerStyles = StyleSheet.create({
     width: SIZE,
     height: SIZE,
     borderRadius: Math.ceil(SIZE / 2),
-    margin: (HEADING_BOX_SIZE - SIZE) / 2,
-  },
+    margin: (HEADING_BOX_SIZE - SIZE) / 2
+  }
 });
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'flex-end',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFillObject
   },
   bubble: {
     backgroundColor: 'rgba(255,255,255,0.7)',
     paddingHorizontal: 18,
     paddingVertical: 12,
-    borderRadius: 20,
+    borderRadius: 20
   },
   latlng: {
     width: 200,
-    alignItems: 'stretch',
+    alignItems: 'stretch'
   },
   button: {
     width: 100,
     paddingHorizontal: 8,
     alignItems: 'center',
     justifyContent: 'center',
-    marginHorizontal: 5,
+    marginHorizontal: 5
   },
   buttonContainer: {
     flexDirection: 'row',
     marginVertical: 20,
-    backgroundColor: 'transparent',
+    backgroundColor: 'transparent'
   },
   buttonText: {
-    textAlign: 'center',
-  },
+    textAlign: 'center'
+  }
 });
 
 const mapStyles = StyleSheet.create({
